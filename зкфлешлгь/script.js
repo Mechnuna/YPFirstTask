fc9b79b4094e1891a58a82a4e77afff96114e577
Mariia <masya.kim@mail.ru>
2021-12-19 18:21:07 +0300

    index add

---
+++ зкфлешлгь/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+    $(".slider").slick(
+        {
+            infinite: true,
+            arrows: false,
+            slidesToScroll: 3,
+            variableWidth: true
+        }
+    );
+});
